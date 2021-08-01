3d33d19defc80905129755d1d6af819e57d221b5
Faizan Ansari <77899660+faizanansarigithub@users.noreply.github.com>
2021-08-01 23:21:07 +0530

    Cookies Basic

--- DynamicFormApp2-ST-Using-Session/src/com/fa/servlet/FirstServlet.java
+++ DynamicFormApp2-ST-Using-Session/src/com/fa/servlet/FirstServlet.java
@@ -5,7 +5,6 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,7 +14,6 @@ public class FirstServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		PrintWriter pw=null;
 		String name=null,addrs=null,ms=null;
-		Cookie ck1=null,ck2=null,ck3=null;
 		//get PrintWriter
 		pw=res.getWriter();
 		//set response content type
@@ -24,11 +22,6 @@ public class FirstServlet extends HttpServlet {
 		name=req.getParameter("pname");
 		addrs=req.getParameter("paddrs");
 		ms=req.getParameter("ms");
-		//use cookies
-		ck1=new Cookie("cname", name);
-		ck2=new Cookie("caddrs", addrs);
-		ck3=new Cookie("cms", ms);
-		res.addCookie(ck1);res.addCookie(ck2);res.addCookie(ck3);
 		if(ms==null)
 			ms="unmarride";
 		
@@ -39,6 +32,7 @@ public class FirstServlet extends HttpServlet {
 			pw.println("<tr><td>spouse name:::</td<td><input type='text'name='f2txt1'/></td></tr>");
 			pw.println("<tr><td>No. of kids:::</td<td><input type='text'name='f2txt2'/></td></tr>");
 			pw.println("<tr><td colspan='2'><input type='submit' value='submit'/></td></tr>");
+			pw.println("<tr><td colspan='2'><input type='hidden' name='hname' value='"+name+"'/><input type='hidden' name='haddrs' value='"+addrs+"'/><input type='hidden' name='hms' value='"+ms+"'/></td></tr>");
 			pw.println("</table>");
 			pw.println("</form>");
 		}
@@ -49,6 +43,7 @@ public class FirstServlet extends HttpServlet {
 			pw.println("<tr><td>When You Want to Marry:::</td<td><input type='text'name='f2txt1'/></td></tr>");
 			pw.println("<tr><td>Why You Want to Marry:::</td<td><input type='text'name='f2txt2'/></td></tr>");
 			pw.println("<tr><td colspan='2'><input type='submit' value='submit'/></td></tr>");
+			pw.println("<tr><td colspan='2'><input type='hidden' name='hname' value='"+name+"'/><input type='hidden' name='haddrs' value='"+addrs+"'/><input type='hidden' name='hms' value='"+ms+"'/></td></tr>");
 			pw.println("</table>");
 			pw.println("</form>");
 		}
--- DynamicFormApp2-ST-Using-Session/src/com/fa/servlet/SecondServlet.java
+++ DynamicFormApp2-ST-Using-Session/src/com/fa/servlet/SecondServlet.java
@@ -5,7 +5,6 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,25 +17,20 @@ public class SecondServlet extends HttpServlet {
 		PrintWriter pw=null;
 		String name=null,addrs=null,ms=null;
 		String txt1=null,txt2=null;
-		Cookie ck[]=null;
 		//get PrintWriter
 		pw=res.getWriter();
 		//set response content type
 		res.setContentType("text/html");
 		//read data from req obj
-		ck=req.getCookies();
-		if(ck!=null) {
-			name=ck[0].getValue();
-			addrs=ck[1].getValue();
-			ms=ck[2].getValue();
-		}
+		name=req.getParameter("hname");
+		addrs=req.getParameter("haddrs");
+		ms=req.getParameter("hms");
 		
-		
-	
 		txt1=req.getParameter("f2txt1");
-		txt2=req.getParameter("f2txt2");
-		pw.println("Name is "+name +" Address "+addrs+" Marriage Status "+ms+"<br>");
-		pw.println("spouse name "+txt1 +" Number Of Kids "+txt2+"<br>");
+		txt2=req.getParameter("f2txt2");	
+		
+		pw.println("Name is:: "+name +" Address:: "+addrs+"Married Status:: "+ms+"<br>");
+		pw.println("spouse name ::"+txt1 +" Number Of Kids:: "+txt2+"<br>");
 		//close the stream
 		pw.close();
 	}
